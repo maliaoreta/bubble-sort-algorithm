b0eb7a09d5bb945ae6af7a01837f1a105eeb67de
maliaoreta <maliaoreta@gmail.com>
2016-03-29 09:50:52 -1000

    tests if bubbleSort is a func and if it takes and returns an Array argument

--- public/js/tests/bubble-sort.spec.js
+++ public/js/tests/bubble-sort.spec.js
@@ -1,7 +1,20 @@
-describe('Function exists', function () {
+describe('bubbleSort', function () {
 
-  it('should do something', function () {
+  // var bubbleTest = null;
 
-    expect(aFunc).to.exist;
+  // beforeEach(function () {
+
+  //   bubbleTest = new bubbleSort();
+  // });
+
+  it('should be a function', function () {
+
+    expect(bubbleSort).to.be.a('function');
+  });
+
+  it('should take an Array as an argument', function () {
+
+    var testArr = [1,2,3];
+    expect(bubbleSort(testArr)).to.equal(testArr);
   })
-})
+});
