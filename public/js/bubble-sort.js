f8176784b4d2def8057c1983903c6f88de9a4435
maliaoreta <maliaoreta@gmail.com>
2016-03-29 15:26:11 -1000

    returns swap count rather than sorted array

--- public/js/bubble-sort.js
+++ public/js/bubble-sort.js
@@ -1,4 +1,28 @@
 function bubbleSort (arr) {
 
-  return arr;
+  var swapped = true;
+  var swapCount = 0;
+
+  while (swapped === true) {
+
+    swapped = false;
+
+    for (var i = 0; i < arr.length-1; i++) {
+
+      var curr = arr[i];
+      var next = arr[i+1];
+
+      if (curr > next) {
+        arr[i] = next;
+        arr[i+1] = curr;
+        swapCount += 1;
+        swapped = true;
+      }    
+    }
+  }
+
+  this.arr = arr;
+  console.log('swapCount', swapCount);
+  return swapCount;
 }
+
